6835929dc4b838b64bc103968474b7520543f966
Moxcar <moxcar99@gmail.com>
2024-08-20 22:55:38 -0600

    chore: Refactor authorization check in cron-jobs route

--- src/app/api/cron-jobs/route.ts
+++ src/app/api/cron-jobs/route.ts
@@ -7,7 +7,7 @@ import { api } from "~/trpc/server";
 
 export async function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}` && false) {
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response("Unauthorized", {
       status: 401,
     });
