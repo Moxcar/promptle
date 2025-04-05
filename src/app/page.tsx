b7e94eae23fe0e9b23c78a13d752114c3104c9f4
Moxcar <moxcar99@gmail.com>
2025-04-04 23:36:56 -0600

    Wrap components in a div for improved layout structure in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,11 @@ export default async function Home() {
   } = await api.dailyImage.getImageUrlOfTheDay();
   return (
     <HydrateClient>
-      <DailyImage imageUrl={imageUrl} />
-      <GuessesInputs length={wordLength} />
-      <Keyboard length={wordLength} dailyImageGuessId={dailyImageGuessId} />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <DailyImage imageUrl={imageUrl} />
+        <GuessesInputs length={wordLength} />
+        <Keyboard length={wordLength} dailyImageGuessId={dailyImageGuessId} />
+      </div>
     </HydrateClient>
   );
 }
