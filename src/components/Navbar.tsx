a77b1f7ed40d9bd07328ce48fd24727faaf27a95
Moxcar <moxcar99@gmail.com>
2025-06-01 22:07:23 -0600

    Update Navbar background color for improved visibility in light mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "./ModeToggle";
 
 function NavBar() {
   return (
-    <nav className="absolute top-0 z-10 flex w-full items-center justify-between justify-self-start border-b border-black/[0.1] bg-white/30 px-20 py-2 backdrop-blur-md dark:border-white/[0.1] dark:bg-gray-800/30">
+    <nav className="absolute top-0 z-10 flex w-full items-center justify-between justify-self-start border-b border-black/[0.1] bg-gray-300/30 px-20 py-2 backdrop-blur-md dark:border-white/[0.1] dark:bg-gray-800/30">
       <div className="flex items-center gap-4">
         <a href="/" className="text-xl font-bold">
           Promptle
