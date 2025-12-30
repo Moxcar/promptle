783a0d4a3da12e66a6000849580c5bf6f10a60ae
Moxcar <moxcar99@gmail.com>
2025-12-30 00:22:44 -0600

    Refactor headers import and type handling in tRPC context creation

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,7 +1,7 @@
 import "server-only";
 
 import { createHydrationHelpers } from "@trpc/react-query/rsc";
-import { headers, type UnsafeUnwrappedHeaders } from "next/headers";
+import { headers } from "next/headers";
 import { cache } from "react";
 
 import { createCaller, type AppRouter } from "~/server/api/root";
@@ -13,7 +13,7 @@ import { createQueryClient } from "./query-client";
  * handling a tRPC call from a React Server Component.
  */
 const createContext = cache(() => {
-  const heads = new Headers((headers() as unknown as UnsafeUnwrappedHeaders));
+  const heads = new Headers(headers());
   heads.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
@@ -26,5 +26,5 @@ const caller = createCaller(createContext);
 
 export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
   caller,
-  getQueryClient
+  getQueryClient,
 );
